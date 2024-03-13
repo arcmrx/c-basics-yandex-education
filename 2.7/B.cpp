a577a38bed9f8404fa8451fe6707a6acc00d7a61
arcmrx <arturvolkov44@gmail.com>
2024-03-13 22:02:25 +0300

    B - Done

---
+++ 2.7/B.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string CommonPrefix(const std::vector<std::string> &words)
+{
+    if (words.empty())
+    {
+        return "";
+    }
+
+    std::string shortest = words[0];
+    for (const std::string &word : words)
+    {
+        if (word.length() < shortest.length())
+        {
+            shortest = word;
+        }
+    }
+
+    for (size_t i = 0; i < shortest.length(); ++i)
+    {
+        char current_char = shortest[i];
+        for (const std::string &word : words)
+        {
+            if (word[i] != current_char)
+            {
+                return shortest.substr(0, i);
+            }
+        }
+    }
+
+    return shortest;
+}
